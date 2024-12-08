74ac82a3216cb4a88fdf60b2a287d4e8d5b44bd0
Alay Naru <alay@zocket.com>
2024-12-08 11:22:55 +0530

    project updated

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
-
-
 gsap.registerPlugin(ScrollTrigger);
 
 const cursor = document.getElementById("cursor");
-const cursorWrapper = document.getElementById("cursor-wrapper")
+const cursorWrapper = document.getElementById("cursor-wrapper");
 
 window.addEventListener("mousemove", (e) => {
-  const cursorWrapperWidth = cursorWrapper.getBoundingClientRect().width ;
-  cursorWrapper.style.transform = `translate(${e.clientX - cursorWrapperWidth / 2}px, ${e.clientY - cursorWrapperWidth / 2}px)`;
+  const cursorWrapperWidth = cursorWrapper.getBoundingClientRect().width;
+  cursorWrapper.style.transform = `translate(${
+    e.clientX - cursorWrapperWidth / 2
+  }px, ${e.clientY - cursorWrapperWidth / 2}px)`;
   cursor.style.transform = `translate(${e.clientX - 6}px, ${e.clientY - 6}px) `;
-})
+});
 
 window.scrollTo(0, 0);
 
-// hero section 
+// hero section
 
 const tl = gsap.timeline();
 tl?.from("#hello-text", {
   opacity: 0,
   duration: 2,
   delay: 1,
-  display: "flex"
+  display: "flex",
 });
 
 tl?.from("#i-am-text", {
   opacity: 0,
   duration: 1,
-  display: "flex"
+  display: "flex",
 });
-
 
 tl?.from("#alay-text", {
   opacity: 0,
   display: "flex",
-  duration: 3
+  duration: 3,
 });
-
-
 
 tl?.from("#alay-name", {
   opacity: 0,
   x: -100,
-
-})
-
+});
 
 tl?.from("#tagline-1", {
   opacity: 0,
@@ -66,12 +61,9 @@ tl?.from("#tagline-3", {
   y: 20,
 });
 
-
 tl?.from("#scroll-gif", {
   opacity: 0,
-
-})
-
+});
 
 tl?.to("#alay-name", {
   opacity: 1,
@@ -109,7 +101,6 @@ tl?.to("#alay-name", {
 //   },
 // });
 
-
 // gsap?.to("#scroll-gif", {
 //   opacity: 0,
 //   stagger: 0.2,
@@ -123,84 +114,75 @@ tl?.to("#alay-name", {
 //   },
 // });
 
+// experince section
 
-
-
-// experince section 
-
-
-gsap?.from("#text-1 span", {
+const experienceTextEffect = {
   opacity: 0.2,
+  filter: "blur(10px)",
   stagger: 0.05,
   y: 50,
+  x: 120,
+};
+
+gsap?.from("#text-1 span", {
+  ...experienceTextEffect,
   scrollTrigger: {
     trigger: "#text-1",
     start: "top 80%",
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#text-2 span", {
-  opacity: 0.2,
-  stagger: 0.05,
-  y: 50,
+  ...experienceTextEffect,
   scrollTrigger: {
     trigger: "#text-2",
     start: "top 80%",
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#text-3 span", {
-  opacity: 0.2,
-  stagger: 0.05,
-  y: 50,
+  ...experienceTextEffect,
   scrollTrigger: {
     trigger: "#text-3",
     start: "top 80%",
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#text-4 span", {
-  opacity: 0.2,
-  stagger: 0.05,
-  y: 50,
+  ...experienceTextEffect,
   scrollTrigger: {
     trigger: "#text-4",
     start: "top 80%",
     end: "top 20%",
     scrub: true,
   },
-})
-
+});
 
 gsap?.from("#text-5 span", {
-  opacity: 0.2,
-  stagger: 0.05,
-  y: 50,
+  ...experienceTextEffect,
   scrollTrigger: {
     trigger: "#text-5",
     start: "top 80%",
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#text-6 span", {
-  opacity: 0.2,
-  stagger: 0.05,
-  y: 50,
+  ...experienceTextEffect,
   scrollTrigger: {
     trigger: "#text-6",
     start: "top 80%",
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#exp-1 ", {
   opacity: 0.2,
@@ -212,7 +194,7 @@ gsap?.from("#exp-1 ", {
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#exp-2 ", {
   opacity: 0.2,
@@ -223,7 +205,7 @@ gsap?.from("#exp-2 ", {
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#exp-3 ", {
   opacity: 0.2,
@@ -234,7 +216,7 @@ gsap?.from("#exp-3 ", {
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#exp-4 ", {
   opacity: 0.2,
@@ -245,9 +227,9 @@ gsap?.from("#exp-4 ", {
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
-// featured works 
+// featured works
 
 gsap?.from("#featured-work-1 span", {
   opacity: 0.2,
@@ -259,32 +241,33 @@ gsap?.from("#featured-work-1 span", {
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#featured-work-2 span", {
   opacity: 0.2,
   stagger: 0.05,
   y: 50,
+
   scrollTrigger: {
     trigger: "#featured-work-2",
     start: "top 80%",
     end: "top 20%",
     scrub: true,
   },
-})
+});
 
 gsap?.from("#featured-work-3 span", {
   opacity: 0.2,
   stagger: 0.05,
   y: 50,
+
   scrollTrigger: {
     trigger: "#featured-work-3",
     start: "top 80%",
     end: "top 20%",
     scrub: true,
   },
-})
-
+});
 
 gsap?.from("#project-1 ", {
   opacity: 0.2,
@@ -295,9 +278,8 @@ gsap?.from("#project-1 ", {
     start: "top 80%",
     end: "top 40%",
     scrub: true,
-
   },
-})
+});
 
 gsap?.from("#project-2 ", {
   opacity: 0.2,
@@ -308,9 +290,8 @@ gsap?.from("#project-2 ", {
     start: "top 80%",
     end: "top 40%",
     scrub: true,
-
   },
-})
+});
 
 gsap?.from("#project-3 ", {
   opacity: 0.2,
@@ -321,77 +302,74 @@ gsap?.from("#project-3 ", {
     start: "top 80%",
     end: "top 40%",
     scrub: true,
-
   },
-})
+});
 
-//cursor change 
+//cursor change
 
-const work1 = document.getElementById("project-1") ;
+const work1 = document.getElementById("project-1");
 work1.addEventListener("mouseenter", () => {
-  cursor.style.display = "none" ;
+  cursor.style.display = "none";
   cursorWrapper.innerText = "View";
-  cursorWrapper.style.background= "#fff";
-  cursorWrapper.style.width= "5rem";
-  cursorWrapper.style.height= "5rem";
-})
+  cursorWrapper.style.background = "#fff";
+  cursorWrapper.style.width = "5rem";
+  cursorWrapper.style.height = "5rem";
+});
 work1.addEventListener("mouseleave", () => {
-  cursor.style.display = "flex" ;
+  cursor.style.display = "flex";
   cursorWrapper.innerText = "";
-  cursorWrapper.style.background= "transparent";
-  cursorWrapper.style.width= "3.5rem";
-  cursorWrapper.style.height= "3.5rem";
-})
+  cursorWrapper.style.background = "transparent";
+  cursorWrapper.style.width = "3.5rem";
+  cursorWrapper.style.height = "3.5rem";
+});
 
-const work2 = document.getElementById("project-2") ;
+const work2 = document.getElementById("project-2");
 work2.addEventListener("mouseenter", () => {
-  cursor.style.display = "none" ;
+  cursor.style.display = "none";
   cursorWrapper.innerText = "View";
-  cursorWrapper.style.background= "#fff";
-  cursorWrapper.style.width= "5rem";
-  cursorWrapper.style.height= "5rem";
-})
+  cursorWrapper.style.background = "#fff";
+  cursorWrapper.style.width = "5rem";
+  cursorWrapper.style.height = "5rem";
+});
 work2.addEventListener("mouseleave", () => {
-  cursor.style.display = "flex" ;
+  cursor.style.display = "flex";
   cursorWrapper.innerText = "";
-  cursorWrapper.style.background= "transparent";
-  cursorWrapper.style.width= "3.5rem";
-  cursorWrapper.style.height= "3.5rem";
-})
+  cursorWrapper.style.background = "transparent";
+  cursorWrapper.style.width = "3.5rem";
+  cursorWrapper.style.height = "3.5rem";
+});
 
-const work3 = document.getElementById("project-3") ;
+const work3 = document.getElementById("project-3");
 work3.addEventListener("mouseenter", () => {
-  cursor.style.display = "none" ;
+  cursor.style.display = "none";
   cursorWrapper.innerText = "View";
-  cursorWrapper.style.background= "#fff";
-  cursorWrapper.style.width= "5rem";
-  cursorWrapper.style.height= "5rem";
-
-})
+  cursorWrapper.style.background = "#fff";
+  cursorWrapper.style.width = "5rem";
+  cursorWrapper.style.height = "5rem";
+});
 work3.addEventListener("mouseleave", () => {
-  cursor.style.display = "flex" ;
+  cursor.style.display = "flex";
   cursorWrapper.innerText = "";
-  cursorWrapper.style.background= "transparent";
-  cursorWrapper.style.width= "3.5rem";
-  cursorWrapper.style.height= "3.5rem";
-})
+  cursorWrapper.style.background = "transparent";
+  cursorWrapper.style.width = "3.5rem";
+  cursorWrapper.style.height = "3.5rem";
+});
 
 const cursorInvertElements = document.querySelectorAll(".invert-cursor");
 
- Array.from(cursorInvertElements).map(element => {
-
-  element.addEventListener("mouseenter",  () => {
-    cursorWrapper.style.background= "rgb(255,255,255)";
-    cursorWrapper.style.mixBlendMode= "exclusion";
-    cursor.style.display = "none" ;
-    cursorWrapper.style.width= "5rem";
-    cursorWrapper.style.height= "5rem";
-  })
-  element.addEventListener("mouseleave",  () => {
-    cursorWrapper.style.background= "transparent";
-    cursorWrapper.style.mixBlendMode= "normal";
-    cursor.style.display = "flex" ;
-    cursorWrapper.style.width= "3.5rem";
-    cursorWrapper.style.height= "3.5rem";
-  })
-})
+Array.from(cursorInvertElements).map((element) => {
+  element.addEventListener("mouseenter", () => {
+    cursorWrapper.style.background = "rgb(255,255,255)";
+    cursorWrapper.style.mixBlendMode = "exclusion";
+    cursor.style.display = "none";
+    cursorWrapper.style.width = "5rem";
+    cursorWrapper.style.height = "5rem";
+  });
+  element.addEventListener("mouseleave", () => {
+    cursorWrapper.style.background = "transparent";
+    cursorWrapper.style.mixBlendMode = "normal";
+    cursor.style.display = "flex";
+    cursorWrapper.style.width = "3.5rem";
+    cursorWrapper.style.height = "3.5rem";
+  });
+});
